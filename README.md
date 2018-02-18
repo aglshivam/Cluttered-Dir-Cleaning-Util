6eb336214e7f4c9250f874dae81b94ab9507cc58
aglshivam <shivamagrawal68cse@gmail.com>
2018-02-18 14:54:38 +0530

    Take path from user

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ This Utility has two parts-
 1. The utility will scan all the directory for the user recursively and
    display top 10 largest files on the system
 
-2. The utility will sort the files on Desktop on the basis of file extension and move them in
-   separate folders in Documents folder. For example: If there are 10 MP3 files, 3 word documents, 4 PDFs then in Documents Folder three folders should be created with name MP3, DOC and PDF and all the files on the Desktop should be moved into these directories based on their file type or extension. Desktop will be cleaned but it will not remove folders and shortcuts of any application.
+2. The utility will sort the files on path/Desktop on the basis of file extension and move them in
+   separate folders in Documents folder. For example: If there are 10 MP3 files, 3 word documents, 4 PDFs then in Documents Folder three folders should be created with name MP3, DOC and PDF and all the files on the path/Desktop should be moved into these directories based on their file type or extension. Desktop/path will be cleaned but it will not remove folders and shortcuts of any application.
--- innov.py
+++ innov.py
@@ -1,6 +1,9 @@
 """
-First part - identify the top 10 sized files in the file system. No input is required.
-Second part - would clean the cluttered desktop 
+First part - identify the top 10 sized files in the input path recursively.
+Input format path - /home/user*
+
+Second part - would clean the cluttered directory provided by the user
+Input format path - /home/user/Desktop
 """
 
 import os
@@ -8,8 +11,21 @@ from collections import defaultdict
 from os.path import join, getsize
 
 #get the current user path
-current_user = os.path.expanduser('~')
+
+inp = raw_input("Enter the Path to get 10 highest size files\n")
+while True:
+	if not os.path.exists(inp):
+		print "Invalid!!!"
+		print "path format required- /home/username*"
+		inp = raw_input("Try Again!!!\n")
+	else:
+		break
+
+#current_user = os.path.expanduser('~')
+current_user = inp
 result = []
+
+print "Processing..."
 
 """
 Walk through the directory recursively and scan the files
@@ -36,11 +52,21 @@ for a,b in result:
 	print "path : " ,b
 	print "-------------------------------------------------"
 
+print "\n"
 
-print "Desktop Cleaning........"
+inp = raw_input("Enter the cluterred directory path\n")
+while True:
+	if not os.path.exists(inp):
+		print "Invalid!!!"
+		print "path format- /home/username*"
+		inp = raw_input("Try Again!!!\n")
+	else:
+		break
 
+print  "Cleaning........"
+current_user = inp
 d = defaultdict(list)
-for root, dirs, files in os.walk(current_user +'/Desktop'):
+for root, dirs, files in os.walk(current_user):
 	for name in files:
 		tempfilename, file_extension = os.path.splitext(name)
 		#get the filename and extension of the file
@@ -53,17 +79,17 @@ for root, dirs, files in os.walk(current_user +'/Desktop'):
 	break
 
 #move the files from source to destionation
-doc_path = current_user +'/Documents/'
-current = current_user +'/Desktop/'
+doc_path = os.path.expanduser('~') +'/Documents/'
+current = current_user +"/"
 for a,b in d.items():
 	#create a folder of extension name
-	newpath = doc_path + a[1:].upper() +"/"
+	newpath = doc_path + a[1:].upper() 
 	#no need to create the folder if it is already present
 	if not os.path.exists(newpath):
 		os.makedirs(newpath)
 	for i in b:
-		desk = current + i + a
+		desk = current +i + a
 		dest = newpath + i + a
 		os.rename(desk, dest)
 
-print "Desktop is cleaned"
+print "Cleaned. Check your directory ",doc_path
